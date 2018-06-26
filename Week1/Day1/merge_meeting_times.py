0d190e7d3e0987c9bb3d93f5c51e76646e381753
muralimohanreddy319 <31232897+muralimohanreddy319@users.noreply.github.com>
2018-06-26 11:03:59 +0530

    Initial commit

---
+++ Week1/Day1/merge_meeting_times.py
@@ -0,0 +1,81 @@
+import unittest
+
+
+def merge_ranges(meetings):
+
+    # Merge meeting ranges
+    meetings.sort()
+    print meetings
+    merged_meetings=[]
+    merged_meetings.insert(0,meetings[0])
+    print merged_meetings
+    for i in range(len(meetings)):
+        temp1 = merged_meetings[-1]
+        temp2 = meetings[i]
+        if(temp1[1]>=temp2[0]):
+            end = max(temp1[1],temp2[1])
+            merged_meetings[-1] = (temp1[0],end)
+        else:
+            merged_meetings.append(temp2)
+        
+    print merged_meetings
+    return merged_meetings
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_meetings_overlap(self):
+        actual = merge_ranges([(1, 3), (2, 4)])
+        expected = [(1, 4)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_touch(self):
+        actual = merge_ranges([(5, 6), (6, 8)])
+        expected = [(5, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meeting_contains_other_meeting(self):
+        actual = merge_ranges([(1, 8), (2, 5)])
+        expected = [(1, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_stay_separate(self):
+        actual = merge_ranges([(1, 3), (4, 8)])
+        expected = [(1, 3), (4, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_multiple_merged_meetings(self):
+        actual = merge_ranges([(1, 4), (2, 5), (5, 8)])
+        expected = [(1, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_not_sorted(self):
+        actual = merge_ranges([(5, 8), (1, 4), (6, 8)])
+        expected = [(1, 4), (5, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_sample_input(self):
+        actual = merge_ranges([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)])
+        expected = [(0, 1), (3, 8), (9, 12)]
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
