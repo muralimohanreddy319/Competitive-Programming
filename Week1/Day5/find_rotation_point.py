740348051dc5d352633d49396d0a05fa2555b4c7
muralimohanreddy319 <31232897+muralimohanreddy319@users.noreply.github.com>
2018-06-29 20:38:01 +0530

    day 5

--- Week1/Day5/find_rotation_point.py
+++ Week1/Day5/find_rotation_point.py
@@ -4,28 +4,27 @@ import unittest
 def find_rotation_point(words):
 
     # Find the rotation point in the list
-    for i in range(len(words)-1):
-        if(words[i]>words[i+1]):
-            return i+1
+    i = BinarySearch(words,0,len(words)-1)
     
-    return -1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return i
+def BinarySearch(words,low,high):
+    while(high>low+1):
+        print high 
+        print low
+        if(words[low]>words[high]):
+            mid = (low+high)//2
+            if(words[mid]>words[high]):
+                low = mid
+            else:
+                high = mid
+        else:
+            return low
+    print words[low]
+    print words[high]
+    if words[low] <= words[high]:
+        return low
+    else:
+        return high
 
 # Tests
 
